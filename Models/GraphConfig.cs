d091dd83f191d047d61e5ac05ea276f535c3ad4a
Josh <j.leikam7@gmail.com>
2020-04-15 14:54:05 -0500

    update settings

--- Models/GraphConfig.cs
+++ Models/GraphConfig.cs
@@ -5,7 +5,7 @@
     public string AppId { get; set; }
     public string AppSecret { get; set; }
     public string TenantId { get; set; }
-    public string Ngrok { get; set; }
+    public string URL { get; set; }
     public string SubscriptionKey { get; set; }
     public string Endpoint { get; set; }
     public string ArchiverId { get; set; }
